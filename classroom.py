8ead9dc43e65fe5f08056bf89e7a12396bdadb64
Robert Lowe <robert.lowe@maryvillecollege.edu>
2018-04-12 15:05:16 -0400

    added a grade report

--- classroom.py
+++ classroom.py
@@ -1,6 +1,6 @@
 from githubpy.github import GitHub
 from pathlib import Path
-from os import chdir,system,mkdir,path
+from os import chdir,system,mkdir,path,getcwd
 
 class Classroom:
   def __init__(self):
@@ -13,7 +13,7 @@ class Classroom:
     self.access_token = Path("access-token").read_text()
     self.gh = GitHub(access_token=self.access_token)
     self.owner = self.gh.user.get().login
-    self.orgname = Path("org-name")
+    self.orgname = Path("org-name").read_text()
 
     #read the roster
     file = open("roster", "r")
@@ -33,5 +33,29 @@ class Classroom:
     Build the source project url for an assignment
     '''
     return "git@github.com:%s/%s %s"%(self.orgname, name)
+  
     
-      
+  def fetch_student_project(self, project, student, owner=None):
+    '''
+    Check for the existence of the project
+    '''
+    #preserve our original path
+    original_dir = getcwd()
+
+    #get the owner name
+    if owner is None:
+      owner = self.orgname
+
+    #change into the sutdent-work directory and then pull the repo
+    chdir('student-work')
+    if not path.isdir(project):
+      mkdir(project)
+    chdir(project)
+    if path.isdir("%s-%s"%(project, student)):
+      chdir("%s-%s"%(project,student))
+      system("git pull")
+    else:
+      system("git clone git@github.com:%s/%s-%s.git"%(owner, project, student))
+
+    #return to the original directory
+    chdir(original_dir)
